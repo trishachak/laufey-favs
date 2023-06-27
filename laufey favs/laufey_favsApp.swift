0b949bc0a2d3e61ac1b74aae50bbb8d76d722cdf
trisha <unipegachicken27@gmail.com>
2023-06-27 13:13:22 -0400

    Initial Commit

---
+++ laufey favs/laufey_favsApp.swift
@@ -0,0 +1,17 @@
+//
+//  laufey_favsApp.swift
+//  laufey favs
+//
+//  Created by Trisha Chakraborty on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct laufey_favsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
